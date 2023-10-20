f046f6a9e21c3713f7fddc2b6f68a154ff7960df
760061 <148262027+760061@users.noreply.github.com>
2023-10-20 09:55:32 -0400

    Update Fraction.java
    fraction class uploaded

--- Fraction.java
+++ Fraction.java
@@ -1,7 +1,31 @@
+import java.until.scanner;
+
 class Fraction{
+Scanner input = new Scanner(System.in);
 int numarator;
 int denominator;
+int whole;
 
+    Fraction(int numaratorVal, int denominatorVal){
+        numarator = numaratorVal;
+        denominator = denominatorVal;
+    }
+
+    Fraction(int wholeVal, int numaratorVal, int denominatorVal){
+        whole = wholeVal;
+        if ( wholeVal<0){
+            numarator = -1* (Math.abs(wholeVal) * denominatorVal + numaratorVal);
+        }else{
+        numarator = wholeVal * denominatorVal+ numaratorVal;
+        }
+        denominator = denominatorVal;
+    }
+
+    Fraction(int wholeVal){
+            whole = wholeVal;
+            numarator = wholeVal;
+            denominator = 1;
+    }
 
 
 
